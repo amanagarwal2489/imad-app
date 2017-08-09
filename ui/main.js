dfdea5c80588821a6eac73d060ebef4c9f74a7cd
amanagarwal2489 <30429578+amanagarwal2489@users.noreply.github.com>
2017-08-09 14:35:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'Hello Welcome to my Web Page';
 
-var img = document.getElementById('main-png');
+var img = document.getElementById('abc');
 img.onclick = function () {
     img.style.marginleft = '100px';
 };

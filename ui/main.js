40448194b30f7453649985aa6933d8ffd4396c0c
amanagarwal2489 <30429578+amanagarwal2489@users.noreply.github.com>
2017-08-09 16:37:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,7 +11,7 @@ function moveRight(){
 }
 img.onclick = function(){
              
-        var interval = setInterval(moveRight, 20);    
+        var interval = setInterval(moveRight, 5);    
 
 };
 

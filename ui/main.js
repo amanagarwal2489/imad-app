f6132c3bd53d1ca8a85dc68adf1cc158cc5421d3
amanagarwal2489 <30429578+amanagarwal2489@users.noreply.github.com>
2017-08-09 16:31:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,14 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'Hello Welcome to my Web Page';
 
 var img = document.getElementById('abc');
-img.onclick = function (){
-    img.style.marginLeft = '100px';
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginleft = marginleft + 'px';
+}
+img.onclick = function(){
+             
+        var interval = setInterval(moveRight, 50);    
+
 };
 

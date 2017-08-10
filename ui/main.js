48aa4038548fbbe1e32a1613723dda477d724be0
amanagarwal2489 <30429578+amanagarwal2489@users.noreply.github.com>
2017-08-10 16:29:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,6 +11,23 @@ function moveRight(){
 img.onclick = function(){
              
         var interval = setInterval(moveRight, 50);    
-
 };
 
+var button = document.getElementById("counter");
+
+button.onclick = function() {
+    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.Done){
+            if (request.Status == 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+       request.open('GET',  'http://aman2489agarwal76.imad.hasura-app.io/counter', true);
+       request.send(null);
+    
+};

94e7c4c52d2d7dd0191dfa489ea96e5b617d5f65
amanagarwal2489 <30429578+amanagarwal2489@users.noreply.github.com>
2017-08-10 12:11:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,11 +89,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/counter', function(req, res) {
-    counter = counter + 1;
-    res.send(counter.toString());
-});
+
 
 app.get('/:articleName', function(req, res){
      var articleName = req.params.articleName;
@@ -108,6 +104,12 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+var counter = 0;
+app.get('/counter', function(req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
 });
 
 app.get('/ui/me.jpg', function (req, res) {
